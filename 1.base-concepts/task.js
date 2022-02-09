4af23d929a9324e729e7354c8d221647ed894337
ДашаY13 <dasha130289@rambler.ru>
2022-02-10 00:00:00 +0300

    initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,23 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  if(a == 0)
+        return false;
+    let arr = {};
+    let D = b * b - 4 * a * c;
+    console.log('D = ' + D);
+    if(D < 0)
+        return false;
+    arr['discriminant'] = D;
+    if(D == 0)
+        arr["quadratic roots"] = (-b + Math.sqrt(D)) / (2 * a);
+    else if(D > 0){
+        let tmp = [];
+        tmp.push((-b + Math.sqrt(D)) / (2 * a));
+        tmp.push((-b - Math.sqrt(D)) / (2 * a));
+        arr["quadratic roots"] = tmp;
+    }
+    return arr;
 }
+  
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
