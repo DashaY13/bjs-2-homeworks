86261c9f0ab5f5a8dbeebec14fed3fa6aca21e42
ДашаY13 <dasha130289@rambler.ru>
2022-02-21 20:26:31 +0300

    initial commit 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,108 +1,39 @@
 // Задание 1
 function getArrayParams(arr) {
-  var arr = [1, 0, -1, -5, 2, -3, 4, 99, -18];
-    function sum(arr) {
-      var arrLen = arr.length,
-          result = 0;
-      for (var i = 0; i < arrLen; i++) {
-        result += arr[i];
-      }
-      return result;
-    }
-    function avg(arr) {
-      var arrLen = arr.length,
-          result = 0;
-      for (var i = 0; i < arrLen; i++) {
-        result += arr[i];
-      }
-      var division = result / arrLen;
-      return division;
-    }
-    var fSum = sum(arr),
-        fAvg = avg(arr);
-  
-    console.log(fSum, fAvg);
-
-    var arr = [1, 0, -1, -5, 2, -3, 4, 99, -18],
-      fMax,
-      fMin;
-  function getMax(arr) {
-    var arrLen = arr.length,
-        maxEl = arr[0];
-    for (var i = 0; i < arrLen; i++) {
-      if (maxEl < arr[i]) {
-        maxEl = arr[i];
-      }
-    }
-    return maxEl;
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+  if(arr[i] < min) min = arr[i];
+  if(arr[i] > max) max = arr[i];
+  sum += arr[i];
   }
-  function getMin(arr) {
-    var arrLen = arr.length,
-        minEl = arr[0];
-    for (var i = 0; i < arrLen; i++) {
-      if (minEl > arr[i]) {
-        minEl = arr[i];
-      }
-    }
-    return minEl;
+  let avg = sum / arr.length;
+  return {min: min, max: max, avg: +(avg.toFixed(2))};
   }
-  fMax = getMax(arr);
-  fMin = getMin(arr);
-  console.log(fMax, fMin); 
-  }
-
 
 // Задание 2
 function worker(arr) {
-  let sum;
-    var arr =[[1, 2, 3, 4], [10, 20, -10, -20]]
-    function sum(arr) {
-      var arrLen = arr.length,
-          result = 0;
-      for (var i = 0; i < arrLen; i++) {
-        result += arr[i];
-      }
-      return result;
-    }
-    return sum;
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+  sum += arr[i];
   }
-  
-  function makeWork(arrOfArr, func) {
-    let max;
-  
-    // Ваш кода
-    // for ...
-    
-    return max;
+  return sum;
+  }
+
+function makeWork(arrOfArr, func) {
+  let max = -Infinity;
+  for(let i = 0; i < arrOfArr.length; i++){
+  let resultOfFunc = func(arrOfArr[i]);
+  if(resultOfFunc > max) max = resultOfFunc;
+  }
+  return max;
   }
   
 
 // Задание 3
+
 function worker2(arr) {
-  console.log(worker([1, 2, 3])); // 6
-console.log(worker([4, 5, 6])); // 15
-let arrOfArr = [[1, 2, 3], [4, 5, 6]];
-makeWork(arrOfArr, worker); // 15
-
-console.log(worker([10, 10, 11])); // 31
-console.log(worker([20, 10])); // 30
-arrOfArr = [[10, 10, 11], [20, 10]];
-makeWork(arrOfArr, worker); // 31
-
-console.log(worker([0, 0, 0])); // 0
-console.log(worker([-1, -100])); // -101
-arrOfArr = [[0, 0, 0], [-1, -100]];
-makeWork(arrOfArr, worker); // 0
-
-//worker2
-console.log(worker2([-10, -20, -40])); // -40 - (-10) = -30 => 30
-console.log(worker2([10, 20, 30])); // 30 - 10 = 20
-arrOfArr = [[-10, -20, -40], [10, 20, 30]];
-makeWork(arrOfArr, worker2); // 30
-
-console.log(worker2([0, 0, 0])); // 0 - 0 = 0
-console.log(worker2([-1, -99])); // -99 - (-1) = -98 => 98
-arrOfArr = [[0, 0, 0], [-1, -99]];
-makeWork(arrOfArr, worker2); // 98
+  let arrParams = getArrayParams(arr);
+  return arrParams.max - arrParams.min;
   }
-
