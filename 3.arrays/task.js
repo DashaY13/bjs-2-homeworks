294e306942ac298d7c587e7ae51b84d8887752c8
ДашаY13 <dasha130289@rambler.ru>
2022-02-22 16:50:14 +0300

    initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,17 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let arr1 = [1, 2, 3, 6, 5];
+  let arr2 = [1, 2, 3, 6, 5]; 
+  const isEqual = (arr1.length === arr2.length) && (arr1.every((value, index) => value === arr2[index]));
+  console.log(isEqual);
 }
+  
 
-function advancedFilter(arr) {
-  let resultArr;
+const arr = [-1, -16, -2, 1, 13, 99]
+.map(function advancedFilter(arr) {
+  if (arr % 3 === 0) {
+    return arr * 10;
+  }
+})
+.filter(i => i > 0);
 
-  // Ваш код
-
-  return resultArr; // array
-}
+console.log(arr)
