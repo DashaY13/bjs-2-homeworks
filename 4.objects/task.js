d292cd3b61a9dfc7cce05e5183d70646f6d629cf
ДашаY13 <dasha130289@rambler.ru>
2022-02-26 12:58:56 +0300

    initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,48 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
+
+let student1 = new Student("Tony", "male", 37);
+let student2 = new Student("Buzz", "female", 35);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+    this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+    if(this.marks === undefined){ 
+        let mark = []
+        mark.unshift("mark 1");
+        // добавить первую оценку 
+        } else {
+            mark.push("mark 2", "mark 3", "mark 4", "mark 5");
+          // добавить вторую и последующие оценки в массив
+        }
+}
+
+Student.prototype.addMarks = function (...marks) {
+    let total = 0;
+    for (let i = 0; i < marks.length; i++) {
+        total += marks[i];
+    }
+    return total;  
+}
+
+Student.prototype.getAverage = function () {
+    const average = mark.reduce((acc, item, idx, arr) => {
+        if (idx === arr.length - 1) {
+            return (acc + item) / arr.length;
+        } else {
+            return acc + item;
+        }
+    })
+    console.log(average);
+}
+
+Student.prototype.exclude = function (reason) {
+ 
+}
+
+
