ec57079cf4ecba14385184249fe2e7863d5c2b32
ДашаY13 <dasha130289@rambler.ru>
2022-02-27 12:06:28 +0300

    initial commit 1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -13,36 +13,27 @@ Student.prototype.setSubject = function (subjectName) {
 
 Student.prototype.addMark = function (mark) {
     if(this.marks === undefined){ 
-        let mark = []
-        mark.unshift("mark 1");
-        // добавить первую оценку 
-        } else {
-            mark.push("mark 2", "mark 3", "mark 4", "mark 5");
-          // добавить вторую и последующие оценки в массив
-        }
-}
-
-Student.prototype.addMarks = function (...marks) {
-    let total = 0;
-    for (let i = 0; i < marks.length; i++) {
-        total += marks[i];
+      this.marks = [];
     }
-    return total;  
-}
+    this.marks.push(mark);
+  }
 
-Student.prototype.getAverage = function () {
-    const average = mark.reduce((acc, item, idx, arr) => {
-        if (idx === arr.length - 1) {
-            return (acc + item) / arr.length;
-        } else {
-            return acc + item;
-        }
-    })
-    console.log(average);
+  Student.prototype.addMarks = function (...marks) {
+    if(this.marks === undefined){ 
+      this.marks = [];
+    }
+    this.marks.push(...marks);
+  }
+
+  Student.prototype.getAverage = function () {
+    let sum = this.marks.reduce((acc, item) => acc + item);
+    return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
- 
-}
+    delete this.marks;
+    delete this.subject;
+    this.excluded = reason;
+  }
 
 
