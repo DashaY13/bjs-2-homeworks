f44e2e39db05acbea025f9a9567009fc5b0a0a3e
ДашаY13 <dasha130289@rambler.ru>
2022-03-12 11:39:22 +0300

    second commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,17 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let arr1 = [1, 2, 3, 6, 5];
-  let arr2 = [1, 2, 3, 6, 5]; 
   const isEqual = (arr1.length === arr2.length) && (arr1.every((value, index) => value === arr2[index]));
-  console.log(isEqual);
+  return isEqual;
 }
-  
 
-const arr = [-1, -16, -2, 1, 13, 99]
-.map(function advancedFilter(arr) {
-  if (arr % 3 === 0) {
-    return arr * 10;
-  }
-})
-.filter(i => i > 0);
-
-console.log(arr)
+function advancedFilter(arr) {
+  const result = arr.filter(x => x > 0).filter(x => x % 3 === 0).map(x => x * 10);
+  return result;
+}
