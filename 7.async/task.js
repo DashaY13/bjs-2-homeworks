5a435a82816ab041f2e8342cfe4858c0370bdd43
ДашаY13 <dasha130289@rambler.ru>
2022-03-12 12:19:08 +0300

    second commit variables

--- 7.async/task.js
+++ 7.async/task.js
@@ -15,34 +15,54 @@ class AlarmClock {
       }
       
     removeClock(id) {
-        let lengthBeforeRemove = this.alarmCollection.length;
-        this.alarmCollection = this.alarmCollection.filter((item) => item.id !== id);
-        return lengthBeforeRemove !== this.alarmCollection.length;
+      let lengthBeforeRemove = this.alarmCollection.length;
+      this.alarmCollection = this.alarmCollection.filter((item) => item.id !== id);
+      return lengthBeforeRemove !== this.alarmCollection.length;
       }
     
     getCurrentFormattedTime(time) {
-        return this.alarmCollection.toTimeString();
-    }
-
+      return new Date().toTimeString().slice(0, 5);
+      }
 
     start() {
-
+      let savedThis = this;
+      function checkClock(item) {
+        if (item.time === savedThis.getCurrentFormattedTime()) {
+        item.callback();
+        }
+      }
+        if(this.timerId === null){
+        this.timerId = setInterval(() => {
+        this.alarmCollection.forEach(elem => checkClock(elem));
+        }, 5000);
+      }
     }
 
     stop() {
-
-    }
+      clearInterval(this.timerId);
+      this.timerId = null;
+      }
 
     printAlarms() {
-      alarmCollection.forEach(id, time);
-        console.log(alarmCollection);
+      this.alarmCollection.forEach(elem => console.log(`Звонок с id = ${elem.id} заведен на время ${elem.time}.`));
       }
     
     clearAlarms() {
+      this.stop();
+      this.alarmCollection = [];
+      }
     }
-  }
-
-
-  const alarmClock = new AlarmClock()
-  this.alarmCollection.push({time: time, id: id, callback: callback});
+    
+  let alarmClock1 = new AlarmClock()
+  alarmClock1.addClock("08:00", () => console.log("Вставай"), 1);
+  alarmClock1.addClock("08:01", () => { console.log("Вставай уже!"); alarmClock1.removeClock(2)}, 2);
+  alarmClock1.addClock("08:01", () => console.log("Вставай проспищь!"));
+  alarmClock1.addClock("08:02", () => {
+    console.log("Вставай, а то уволят!");
+    alarmClock1.clearAlarms();
+    alarmClock1.printAlarms();
+  }, 3);
+  alarmClock1.addClock("08:05", () => console.log("Вставай, а то уволят!"), 1);
+  alarmClock1.printAlarms();
+  alarmClock1.start();
   
